867f5835f6c6c0e083460c542df1e37b3c40cb5a
SergioUsal <rotor.sergio@gmail.com>
2024-04-16 22:26:35 +0200

    Minor changes. Testing with pandas

--- analysis/single-file/single-data.py
+++ analysis/single-file/single-data.py
@@ -7,12 +7,12 @@ import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 # absolute current file path
 # os.chdir(r'C:\Users\sergi\repositorios\gunn-diode-deeplearning-tfg\analysis\single-file')
 
-# relative current file path
+# relative current file path setting
 script_dir = os.path.dirname(os.path.realpath(__file__))
 os.chdir(script_dir)
 
 # read the data file
 datafile = pd.read_fwf("corr01.txt",colspecs=[(0,12),(13,25),(26,38),(39,51),(52,64),(65,77)],names=['Time','V1','V2','V3','V4','V5']);
 
-# print(datafile.iloc[0:10])   # the same as the later one
-print("\n", datafile.head(10))
+print("\n", datafile.head(10))  # print the first 10 rows
+print("\n", datafile.iloc[200000:200100])   # print rows from a to b with iloc[a:b]
