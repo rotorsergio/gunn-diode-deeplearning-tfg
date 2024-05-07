66af6dee78ae614b79a574489d341b52817486df
SergioUsal <rotor.sergio@gmail.com>
2024-05-07 21:01:48 +0200

    Multiprocessiong on file_reducer and other optimizations. Reducing single data accordingly.

--- file_reducer.py
+++ file_reducer.py
@@ -1,27 +1,47 @@
 ﻿import os
-import pandas as pd
+import sys
+import numpy as np
 from tqdm import tqdm # for progress bar
+from multiprocessing import Pool, cpu_count
+
+# Redirect stdout (standard output) and stderr (standard error) to a file
+# sys.stdout = open('output.txt', 'w')
+# sys.stderr = sys.stdout
 
 # Specify the parent directory
 parent_dir = 'D:/datos_montecarlo'
+colspecs = [(56, 69)]
 
 # Get a list of all 'CORR01' files
 files = [os.path.join(dirpath, file)
          for dirpath, dirnames, filenames in os.walk(parent_dir)
          for file in filenames if file == 'CORR01']
 
-
-# Walk through all subdirectories
-for file_path in tqdm(files):
+# Function to process a single file
+def process_file(file_path):
     # Read the file
-    df = pd.read_fwf(file_path)
+    with open(file_path, 'r') as f:
+        data = f.readlines()
 
-    # Select the 5th column (assuming the first column is at index 0)
-    df = df.iloc[:, 4]
+    # Extract the 5th column
+    column5 = [line[colspecs[0][0]:colspecs[0][1]] for line in data]
+
+    # Convert to numpy array and change type to float
+    column5 = np.array(column5, dtype=float)
 
     # Write the selected column to a new file
-    reduced_file_path = os.path.join(os.path.dirname(file_path), 'corr_reduced')
-    df.to_csv(reduced_file_path, index=False)
+    np.savetxt(file_path.replace('CORR01', 'corr_reduced'), column5)
 
-    # Erase the original 'CORR01' file
-    os.remove(file_path)
+# Protect the main part of the code
+if __name__ == '__main__':
+    # Specify the parent directory
+    parent_dir = 'D:/datos_montecarlo'
+
+    # Get a list of all 'CORR01' files
+    files = [os.path.join(dirpath, file)
+             for dirpath, dirnames, filenames in os.walk(parent_dir)
+             for file in filenames if file == 'CORR01']
+
+    # Create a multiprocessing Pool
+    with Pool(cpu_count()) as p:
+        list(tqdm(p.imap(process_file, files), total=len(files)))
